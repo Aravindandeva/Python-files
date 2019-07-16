7e28783af99c0ad9ccdef6d1861d9e49dec84aa6
Aravindandeva <52481398+Aravindandeva@users.noreply.github.com>
2019-07-16 16:25:36 +0530

    Create player s1 5 roman and numerals.py

---
+++ player s1 5 roman and numerals.py
@@ -0,0 +1,6 @@
+numerals=("I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX")
+n=(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
+ji=zip(n,numerals)
+ji=dict(ji)
+w=int(input())
+print(ji[w])
